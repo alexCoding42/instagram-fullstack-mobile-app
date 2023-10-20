fc630d14e3b083dd8a8726bebf50919e03a656b4
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-10-20 10:58:41 +0200

    feat: :sparkles: add theme and fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import colors from './src/themes/colors';
+import fonts from './src/themes/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 function App(): JSX.Element {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello World</Text>
+      <Text style={{color: colors.primary, fontSize: fonts.size.lg}}>
+        Hello World
+        <AntDesign name="stepforward" size={25} />
+      </Text>
     </View>
   );
 }
